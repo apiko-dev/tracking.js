9116f0d5edc9103f9ee2d3fea4bc46eb64e86a82
Michael Rokosh <mike.rokosh@gmail.com>
2015-08-14 13:49:50 +0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'jss:tracking.js',
+  version: '1.1.1',
+  summary: 'Color tracking, face detection and much more.',
+  git: 'https://github.com/eduardolundgren/tracking.js',
+  documentation: 'README.md'
+});
+
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.addFiles([
+    'tracking.js', 
+    'data/eye.js',
+    'data/face.js',
+    'data/mouth.js',
+    'lib/dat.gui.js'
+  ], 'client');
+
+  api.export('tracking', 'client');
+  api.export('dat', 'client');
+});
